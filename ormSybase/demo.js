78094e9be076f58d34d55d74dac0a2c07a8c0e48
Oscar Vicente <ovicente@mediprocesos.com>
2018-05-10 16:38:14 -0600

    demo.js cambio en demo

--- ormSybase/demo.js
+++ ormSybase/demo.js
@@ -12,18 +12,7 @@ SyBase.models.ormDemo.by(SyBase.DBPools.DESA)
   }).then(res => {
     console.info('------- Created ', res)
     //return SyBase.models.WXUser.findByID(res.id)
-  }).create({
-    id: 2,
-    Nombres: 'Oscar',
-    Apellidos: 'Vicente',
-    Telefono: '30347341',
-    Contacto: 'Prueba',
-    status: 'A'
-  }).then(res => {
-    console.info('------- Created ', res)
-    //return SyBase.models.WXUser.findByID(res.id)
-  })
-  .catch(err => {
+  }).catch(err => {
     console.error('********* Error :', err)
     //SyBase.models.WXUser.delete({ cardno: '90000011' })
   })
